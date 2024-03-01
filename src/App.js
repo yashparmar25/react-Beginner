29a481346d1ce99fd824d61b36fbce675a38bec0
yashparmar25 <142145946+yashparmar25@users.noreply.github.com>
2024-03-01 14:35:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h1 className="hello">Hello Everyone</h1>
+      <div className="name">My Name is <span className="yash">Yash</span></div>
+    </div>
+  );
+}
+
+export default App;
